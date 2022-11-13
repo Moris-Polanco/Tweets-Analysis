5cefe86a7e2eb7b6898d5fd2269f4ea08ca40cd2
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2022-11-13 07:51:08 -0600

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-consumer_key = 'CXjWqbqODoTLR8hNwe1h352jX'
-consumer_secret = '5e0c1NR5aQGkHsixpPxxBZulRzxPvRLrMFHI1E3fT0jQnOe7Wh'
-access_key = '113763054-c0ULFNs4rrXthT9FRnooFDVx8C81HVLZJFYBrctG'
-access_secret = 'ZKyUofCpqNRJMfe28xi3pqOCwJ5Pee4e0psIizBZiHXNK'
+self.api_key = 'CXjWqbqODoTLR8hNwe1h352jX'
+self.api_key_secret = '5e0c1NR5aQGkHsixpPxxBZulRzxPvRLrMFHI1E3fT0jQnOe7Wh'
+self.access_token = '113763054-c0ULFNs4rrXthT9FRnooFDVx8C81HVLZJFYBrctG'
+self.access_token_secret = 'ZKyUofCpqNRJMfe28xi3pqOCwJ5Pee4e0psIizBZiHXNK'
